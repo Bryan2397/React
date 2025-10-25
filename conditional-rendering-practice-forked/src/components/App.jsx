437d54b294fe8b356b7a550db5180cdf5cc8d294
Bryan Mirtil <166929786+Bryan2397@users.noreply.github.com>
2025-10-24 23:45:26 -0400

    Refactor App component for conditional rendering

--- conditional-rendering-practice-forked/src/components/App.jsx
+++ conditional-rendering-practice-forked/src/components/App.jsx
@@ -5,25 +5,12 @@ import Button from "./Button";
 
 var userIsRegistered = false;
 
-function renderConditionally() {
-  return !userIsRegistered ? (
-    <form>
-      <Input type="password" placeholder="Username" />
-      <Input type="password" placeholder="Password" />
-      <Input type="password" placeholder="Confirm Password" />
-      <Button type="submit" input="Register" />
-    </form>
-  ) : (
-    <form>
-      <Input type="password" placeholder="Username" />
-      <Input type="password" placeholder="Password" />
-      <Button type="submit" input="Login" />
-    </form>
-  );
-}
-
 function App() {
-  return <div className="container">{renderConditionally()}</div>;
+  return (
+    <div className="container">
+      <Form isRegistered={userIsRegistered} />
+    </div>
+  );
 }
 
 export default App;
