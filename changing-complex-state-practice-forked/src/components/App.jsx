3177c0d6516aa4b2a3d378dfb74f65870eee135f
Bryan Mirtil <166929786+Bryan2397@users.noreply.github.com>
2025-10-26 11:15:34 -0400

    Add files via upload

---
+++ changing-complex-state-practice-forked/src/components/App.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setContact((e) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: e.lName,
+          email: e.email,
+        };
+      } else if (name === "email") {
+        return {
+          fName: e.fName,
+          lName: e.lName,
+          email: value,
+        };
+      } else if (name === "lName") {
+        return {
+          fName: e.fName,
+          lName: value,
+          email: e.email,
+        };
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          placeholder="First Name"
+          value={contact.fName}
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          placeholder="Last Name"
+          value={contact.lName}
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          placeholder="Email"
+          value={contact.email}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
