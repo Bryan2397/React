1c5b7d582fd1fae5ef4e5701a25ee6d60a0c9923
Bryan Mirtil <166929786+Bryan2397@users.noreply.github.com>
2025-10-31 16:07:46 -0400

    Add files via upload

---
+++ managing-a-component-tree-practice-forked/src/components/InputArea.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+function InputArea(props) {
+  return (
+    <div className="form">
+      <input
+        onChange={(e) => props.onChange(e)}
+        type="text"
+        value={props.value}
+      />
+      <button onClick={() => props.change()}>
+        <span>Add</span>
+      </button>
+    </div>
+  );
+}
+
+export default InputArea;
